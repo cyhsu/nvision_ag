267570bc50375ddca33a3f79122866bbf47217fe
C.Y. Hsu <cyhsu.bayer@gmail.com>
2023-06-28 14:13:40 -0500

    Update __init__.py
    ADD: collaborator

--- nvision_ag/__init__.py
+++ nvision_ag/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for NVision AG."""
 
-__author__ = """Chuan-Yuan Hsu"""
-__email__ = 'cyhsu.job@gmail.com'
+__author__ = """Chuan-Yuan Hsu, Tzi-Yi Yang"""
+__email__ = 'cyhsu.job@gmail.com, yangze2065@gmail.com'
 from nvision_ag._version import __version__
